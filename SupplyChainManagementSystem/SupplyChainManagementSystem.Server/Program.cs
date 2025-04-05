be008f1279d6c0468c9dd0885df86871b4613c2d
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-05 17:43:32 -0600

    Forgot to include the EFCore #7

--- SupplyChainManagementSystem/SupplyChainManagementSystem.Server/Program.cs
+++ SupplyChainManagementSystem/SupplyChainManagementSystem.Server/Program.cs
@@ -1,11 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.EntityFrameworkCore;
 using SupplyChainManagementSystem.Server;
+using SupplyChainManagementSystem.Server.DataAccess;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddDbContext<ProductDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 await builder.Build().RunAsync();
