6ab6d539883bd45e5419059d3b8dc7747c2b0644
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-05 08:05:41 -0600

    Copied DataAccess and Models folders #3

---
+++ SupplyChainManagementSystem/SupplyChainManagementSystem.Server/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace SupplyChainManagementSystem.Models
+{
+    public record Product
+    {
+        public Guid Product_Id { get; set; }
+        public string Product_Name { get; set; } = default!;
+        public string Product_Description { get; set; } = default!;
+        public string Product_Category { get; set; } = default!;
+        public decimal Product_Price { get; set; } = default!;
+        public int Product_Quantity { get; set; } = default!;
+        public DateTime Created_At { get; set; } = DateTime.Now;
+    }
+}
