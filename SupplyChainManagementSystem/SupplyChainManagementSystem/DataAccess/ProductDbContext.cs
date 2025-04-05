c0f0eef1130c754ac9918e96d95eebc66cd8d0d8
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-05 06:37:53 -0600

    Added a DbContext class

---
+++ SupplyChainManagementSystem/SupplyChainManagementSystem/DataAccess/ProductDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupplyChainManagementSystem.Models;
+
+namespace SupplyChainManagementSystem.DataAccess
+{
+    public class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options) : base(options)
+        {
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<Product> Products { get; set; } = default!;
+    }
+}
