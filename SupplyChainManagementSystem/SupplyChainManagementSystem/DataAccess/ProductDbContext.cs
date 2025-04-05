3e6b5a75e61755278600bf5dd9793d54f53f9000
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-05 07:35:14 -0600

    This is the GenerateProductData() method VS produced #3

--- SupplyChainManagementSystem/SupplyChainManagementSystem/DataAccess/ProductDbContext.cs
+++ SupplyChainManagementSystem/SupplyChainManagementSystem/DataAccess/ProductDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
 using SupplyChainManagementSystem.Models;
 
 namespace SupplyChainManagementSystem.DataAccess
@@ -18,5 +19,17 @@ namespace SupplyChainManagementSystem.DataAccess
         }
 
         public DbSet<Product> Products { get; set; } = default!;
+        private Product[] GenerateProductData()
+        {
+            var productFaker = new Faker<Product>()
+                .RuleFor(p => p.Product_Id, f => Guid.NewGuid())
+                .RuleFor(p => p.Product_Name, f => f.Commerce.ProductName())
+                .RuleFor(p => p.Product_Description, f => f.Lorem.Sentence(10))
+                .RuleFor(p => p.Product_Category, f => f.Commerce.Categories(1)[0])
+                .RuleFor(p => p.Product_Price, f => decimal.Parse(f.Commerce.Price(1, 1000)))
+                .RuleFor(p => p.Product_Quantity, f => f.Random.Int(1, 100))
+                .RuleFor(p => p.Created_At, f => DateTime.Now);
+        }
     }
+
 }
