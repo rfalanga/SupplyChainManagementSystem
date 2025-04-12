40a2c7dad699f1491d262380bd4d2df505ced126
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-12 07:20:47 -0600

    Beginning to implement the controller

--- SupplyChainManagementSystem/SupplyChainManagementSystem.Server/Controllers/ProductController.cs
+++ SupplyChainManagementSystem/SupplyChainManagementSystem.Server/Controllers/ProductController.cs
@@ -1,8 +1,15 @@
-﻿using System.Web.Mvc;
+﻿using SupplyChainManagementSystem.Server.DataAccess;
+using System.Web.Mvc;
 
 namespace SupplyChainManagementSystem.Server.Controllers
 {
     public class ProductController : Controller
     {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository   )
+        {
+            _productRepository = productRepository;
+        }
     }
 }
