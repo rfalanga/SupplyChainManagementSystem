3498da2c061b4d815c18524518cc98042bc0c6ae
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-16 18:48:34 -0600

    Added Supplier class #12

---
+++ SupplyChainManagementSystem/SupplyChainManagementSystem.Server/Models/Supplier.cs
@@ -0,0 +1,12 @@
+﻿namespace SupplyChainManagementSystem.Server.Models
+{
+    public class Supplier
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+    }
+}
