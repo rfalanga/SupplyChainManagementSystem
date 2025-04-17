df9094563ab7fc767c90c4706e44644b257ffd90
Rod Falanga <rfalanga@users.noreply.github.com>
2025-04-16 18:32:33 -0600

    Finished implementing ProductController

--- SupplyChainManagementSystem/SupplyChainManagementSystem.Server/Controllers/ProductController.cs
+++ SupplyChainManagementSystem/SupplyChainManagementSystem.Server/Controllers/ProductController.cs
@@ -1,7 +1,16 @@
 ﻿using SupplyChainManagementSystem.Server.DataAccess;
 using Microsoft.AspNetCore.Mvc;
+using SupplyChainManagementSystem.Server.Models;
 
 
+/* 
+ * This is from Listing 4 on the CODE Magazine article of March/April 2025
+ * 
+ * The ProductController class is responsible for handling HTTP requests related to products.
+ * It uses the IProductRepository interface to interact with the data layer.
+ * 
+ * The controller has a single action method, GetAllProducts, which returns a list of all products.
+ */
 namespace SupplyChainManagementSystem.Server.Controllers
 {
     [Route("api/[controller]")]
@@ -13,6 +22,12 @@ namespace SupplyChainManagementSystem.Server.Controllers
         public ProductController(IProductRepository productRepository   )
         {
             _productRepository = productRepository;
+        }
+
+        [HttpGet("GetAllProducts")] // In the CODE magazine article this was named GetProducts
+        public async Task<List<Product>> GetAllProducts()
+        {
+            return await _productRepository.GetAllProductsAsync();
         }
     }
 }
